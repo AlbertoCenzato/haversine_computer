6ee5d4c6ca7bcc02aeea9689dbbb8631c1b15540
Alberto Cenzato <alberto.cenzato@outlook.it>
2023-11-25 19:54:02 +0100

    Streaming implementation of next

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,9 @@ fn main() {
     let args = Args::parse();
     println!("Input file {}", args.input.display());
 
-    //let example_json = "{ \"name\": \"John Doe\", \"age\": 43 }";
-    // read input file to string
+    // TODO(alberto): read file in chunks
     let example_json = std::fs::read_to_string(args.input).unwrap();
+    // TODO(alberto): remove this copy
     let json = example_json.chars().collect::<Vec<char>>();
 
     let start = std::time::Instant::now();
@@ -19,198 +19,234 @@ fn main() {
     println!("Tokens: ");
     let mut tokens = Vec::new();
     let mut i = 0;
-    while i < example_json.len() {
-        let token = next(&json, i);
+    let mut state = State::None;
+    while i < json.len() {
+        let (new_state, token, consumed_chars) = next(json[i], i, state);
+        match new_state {
+            State::Error(error) => panic!("Parsing failed with error: {}", error),
+            _ => {
+                state = new_state;
+                i += consumed_chars;
+            }
+        }
+
         match token {
-            Ok(token) => {
-                i = token.end;
-                //println!(
-                //    "{:?}, value: {}",
-                //    &token,
-                //    example_json[token.start..token.end].to_string()
-                //);
-                tokens.push(token);
+            Some(t) => {
+                //println!("{:?}, value {}", t, &example_json[t.start..t.end]);
+                tokens.push(t)
             }
-            Err(description) => {
-                panic!("Parsing failed with error: {}", description)
-            }
+            None => (),
         }
     }
 
     let duration = start.elapsed();
     println!("Time elapsed in while loop is: {:?}", duration);
+    for t in tokens {
+        println!("{:?}, value {}", t, &example_json[t.start..t.end]);
+    }
 }
 
-fn make_simple_token(token_type: TokenType, index: usize) -> Result<Token, String> {
-    return Ok(Token {
+fn make_simple_token(token_type: TokenType, index: usize) -> (State, Option<Token>, usize) {
+    let token = Token {
         token_type: token_type,
         start: index,
         end: index + 1,
-    });
+    };
+    return (State::None, Some(token), 1);
 }
 
-enum Status {
+struct StringState {
+    start_index: usize,
+    escape: bool,
+}
+
+struct NumberState {
+    start_index: usize,
+    decimal_point: bool,
+}
+
+struct LiteralState {
+    start_index: usize,
+    current_index: usize,
+}
+
+enum State {
     None,
-    InString(usize),
-    InNumber(usize),
-    InTrue(usize),
-    InFalse(usize),
-    InNull(usize),
+    InString(StringState),
+    InNumber(NumberState),
+    InTrue(LiteralState),
+    InFalse(LiteralState),
+    InNull(LiteralState),
     Error(String),
 }
 
-fn next(data: &[char], start_index: usize) -> Result<Token, String> {
-    let c = data[start_index];
+fn next(c: char, index: usize, state: State) -> (State, Option<Token>, usize) {
+    match state {
+        State::None => match c {
+            '{' => make_simple_token(TokenType::LeftCurlyBracket, index),
+            '}' => make_simple_token(TokenType::RightCurlyBracket, index),
+            '[' => make_simple_token(TokenType::LeftSquareBracket, index),
+            ']' => make_simple_token(TokenType::RightSquareBracket, index),
+            ':' => make_simple_token(TokenType::Colon, index),
+            ',' => make_simple_token(TokenType::Comma, index),
+            '"' => (
+                State::InString(StringState {
+                    start_index: index,
+                    escape: false,
+                }),
+                None,
+                1,
+            ),
+            't' => (
+                State::InTrue(LiteralState {
+                    start_index: index,
+                    current_index: 1,
+                }),
+                None,
+                1,
+            ),
+            'f' => (
+                State::InFalse(LiteralState {
+                    start_index: index,
+                    current_index: 1,
+                }),
+                None,
+                1,
+            ),
+            'n' => (
+                State::InNull(LiteralState {
+                    start_index: index,
+                    current_index: 1,
+                }),
+                None,
+                1,
+            ),
+            '0'..='9' | '-' => (
+                State::InNumber(NumberState {
+                    start_index: index,
+                    decimal_point: false,
+                }),
+                None,
+                1,
+            ),
+            ' ' | '\n' | '\t' | '\r' => (State::None, None, 1),
+            _ => (
+                State::Error(format!("Ill formed Json at position {}", index)),
+                None,
+                0,
+            ),
+        },
+        State::InString(string_state) => tokenize_string(c, index, string_state),
+        State::InNumber(number_state) => tokenize_number(c, index, number_state),
+        State::InTrue(literal_state) => tokenize_literal(c, index, TokenType::True, literal_state),
+        State::InFalse(literal_state) => {
+            tokenize_literal(c, index, TokenType::False, literal_state)
+        }
+        State::InNull(literal_state) => tokenize_literal(c, index, TokenType::Null, literal_state),
+        State::Error(error) => (State::Error(error), None, 0),
+    }
+}
+
+fn tokenize_string(c: char, index: usize, mut state: StringState) -> (State, Option<Token>, usize) {
+    if state.escape {
+        state.escape = false;
+        return (State::InString(state), None, 1);
+    }
+
     match c {
-        '{' => make_simple_token(TokenType::LeftCurlyBracket, start_index),
-        '}' => make_simple_token(TokenType::RightCurlyBracket, start_index),
-        '[' => make_simple_token(TokenType::LeftSquareBracket, start_index),
-        ']' => make_simple_token(TokenType::RightSquareBracket, start_index),
-        ':' => make_simple_token(TokenType::Colon, start_index),
-        ',' => make_simple_token(TokenType::Comma, start_index),
-        '"' => tokenize_string(&data, start_index),
-        't' => tokenize_true(&data, start_index),
-        'f' => tokenize_false(&data, start_index),
-        'n' => tokenize_null(&data, start_index),
-        '0'..='9' | '-' => tokenize_number(&data, start_index),
-        ' ' | '\n' | '\t' | '\r' => {
-            let index = ignore_whitespace(&data, start_index);
-            match index {
-                Some(index) => next(&data, index),
-                None => Err(format!("EOF reached while ignoring whitespace")),
-            }
+        '"' => {
+            let token = Token {
+                token_type: TokenType::String,
+                start: state.start_index,
+                end: index + 1,
+            };
+            (State::None, Some(token), 1)
         }
-        _ => Err(format!("Ill formed Json at position {}", start_index)),
-    }
-}
-
-fn ignore_whitespace(data: &[char], start_index: usize) -> Option<usize> {
-    for i in start_index..data.len() {
-        match data[i] {
-            ' ' | '\n' | '\t' | '\r' => continue,
-            _ => return Some(i),
-        }
-    }
-    return None;
-}
-
-fn tokenize_string(data: &[char], start_index: usize) -> Result<Token, String> {
-    let first_char = data[start_index];
-    if first_char != '"' {
-        return Err(format!(
-            "Ill formed Json at position {}, expected \", got {}",
-            start_index, first_char
-        ));
-    }
-
-    for i in start_index + 1..data.len() {
-        let c = data[i];
-        match c {
-            '"' => {
-                let prev_char = data[i - 1];
-                if prev_char == '\\' {
-                    continue;
-                }
-                return Ok(Token {
-                    token_type: TokenType::String,
-                    start: start_index,
-                    end: i + 1,
-                });
-            }
-            '\n' => {
-                return Err(format!(
+        '\n' => {
+            return (
+                State::Error(format!(
                     "Ill formed Json at position {}, expected \", got {}",
-                    start_index, c
-                ))
-            }
-            _ => continue,
+                    index, c
+                )),
+                None,
+                0,
+            )
         }
+        _ => return (State::InString(state), None, 1),
     }
-    return Err(format!(
-        "EOF reached while tokenizing string starting at {}",
-        start_index
-    ));
 }
 
-fn tokenize_number(data: &[char], start_index: usize) -> Result<Token, String> {
-    let mut integer = true;
-    for i in start_index..data.len() {
-        let c = data[i];
-        match c {
-            '0'..='9' | '-' => continue,
-            '.' => {
-                if integer {
-                    integer = false;
-                    continue;
-                } else {
-                    return Err(format!(
+fn tokenize_number(c: char, index: usize, mut state: NumberState) -> (State, Option<Token>, usize) {
+    match c {
+        '0'..='9' => (State::InNumber(state), None, 1),
+        '.' => {
+            if state.decimal_point {
+                return (
+                    State::Error(format!(
                         "Ill formed Json at position {}, expected \", got {}",
-                        start_index, c
-                    ));
-                }
-            }
-            _ => {
-                return Ok(Token {
-                    token_type: TokenType::Number,
-                    start: start_index,
-                    end: i + 1,
-                })
+                        index, c
+                    )),
+                    None,
+                    0,
+                );
+            } else {
+                state.decimal_point = true;
+                return (State::InNumber(state), None, 1);
             }
         }
-    }
-    return Err(format!(
-        "EOF reached while parsing for number starting at {}",
-        start_index
-    ));
-}
-
-fn equals(data: &[char], literal: &str) -> bool {
-    if data.len() != literal.len() {
-        return false;
-    }
-    for i in 0..literal.len() {
-        if data[i] != literal.as_bytes()[i] as char {
-            return false;
+        _ => {
+            let token = Token {
+                token_type: TokenType::Number,
+                start: state.start_index,
+                end: index,
+            };
+            return (State::None, Some(token), 0);
         }
     }
-    return true;
 }
 
 fn tokenize_literal(
-    data: &[char],
-    literal: &str,
+    c: char,
+    index: usize,
     token_type: TokenType,
-    start_index: usize,
-) -> Result<Token, String> {
-    if data.len() < start_index + literal.len() {
-        return Err(format!(
-            "EOF reached while parsing for '{}' literal starting at {}",
-            literal, start_index
-        ));
+    mut state: LiteralState,
+) -> (State, Option<Token>, usize) {
+    let literal = match token_type {
+        TokenType::True => "true",
+        TokenType::False => "false",
+        TokenType::Null => "null",
+        _ => panic!("Invalid token type"),
+    };
+    let expected = literal.as_bytes()[state.current_index] as char;
+    if c != expected {
+        return (
+            State::Error(format!(
+                "Failed tokenizing literal {} at position {}, expected {}, found {}",
+                &literal,
+                state.start_index + state.current_index,
+                expected,
+                c
+            )),
+            None,
+            0,
+        );
     }
-    if equals(&data[start_index..start_index + literal.len()], literal) {
-        return Ok(Token {
+    if state.current_index == literal.len() - 1 {
+        let token = Token {
             token_type: token_type,
-            start: start_index,
-            end: start_index + literal.len(),
-        });
+            start: state.start_index,
+            end: index,
+        };
+        return (State::None, Some(token), 1);
     }
-    return Err(format!(
-        "Could not parse '{}' literal at position {}",
-        literal, start_index
-    ));
-}
 
-fn tokenize_true(data: &[char], start_index: usize) -> Result<Token, String> {
-    tokenize_literal(data, "true", TokenType::True, start_index)
-}
-
-fn tokenize_false(data: &[char], start_index: usize) -> Result<Token, String> {
-    tokenize_literal(data, "false", TokenType::False, start_index)
-}
-
-fn tokenize_null(data: &[char], start_index: usize) -> Result<Token, String> {
-    tokenize_literal(data, "null", TokenType::Null, start_index)
+    state.current_index += 1;
+    return match token_type {
+        TokenType::True => (State::InTrue(state), None, 1),
+        TokenType::False => (State::InFalse(state), None, 1),
+        TokenType::Null => (State::InNull(state), None, 1),
+        _ => panic!("Invalid token type"),
+    };
 }
 
 #[derive(Debug, PartialEq)]
@@ -241,54 +277,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_tokenize_true() {
-        let token = tokenize_true("true", 0);
-        let token = token.unwrap();
-        assert_eq!(token.token_type, TokenType::True);
-        assert_eq!(token.start, 0);
-        assert_eq!(token.end, 4);
-
-        let token = tokenize_true("True", 0);
-        assert!(token.is_err());
-    }
+    fn test_tokenize_true() {}
 
     #[test]
-    fn test_tokenize_false() {
-        let token = tokenize_false("false", 0);
-        let token = token.unwrap();
-        assert_eq!(token.token_type, TokenType::False);
-        assert_eq!(token.start, 0);
-        assert_eq!(token.end, 5);
-
-        let token = tokenize_false(" false", 0);
-        assert!(token.is_err());
-    }
+    fn test_tokenize_false() {}
 
     #[test]
-    fn test_tokenize_null() {
-        let token = tokenize_null("null", 0);
-        let token = token.unwrap();
-        assert_eq!(token.token_type, TokenType::Null);
-        assert_eq!(token.start, 0);
-        assert_eq!(token.end, 4);
-
-        let token = tokenize_null("nul", 0);
-        assert!(token.is_err());
-    }
+    fn test_tokenize_null() {}
 
     #[test]
-    fn test_tokenize_string() {
-        let token = tokenize_string("\"hell15896_\t o\"", 0);
-        let token = token.unwrap();
-        assert_eq!(token.token_type, TokenType::String);
-        assert_eq!(token.start, 0);
-        assert_eq!(token.end, 14);
-
-        let token = tokenize_string("hello\"", 0);
-        assert!(token.is_err());
-        let token = tokenize_string("\"hel\nlo\"", 0);
-        assert!(token.is_err());
-        let token = tokenize_string("\"hello", 0);
-        assert!(token.is_err());
-    }
+    fn test_tokenize_string() {}
 }
